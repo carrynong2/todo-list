5f96b58ca1174f361c5f944f1e9d81960d984b69
carrynong2 <carrynong2@gmail.com>
2023-10-07 23:13:18 +0700

    add Todo.tsx

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,6 +1,23 @@
-const Todo = () => {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+
+type TaskProps = {
+  id: any
+  task: string;
+  completed: boolean;
+  isEditing: boolean;
+}
+
+const Todo = ({ task }: TaskProps) => {
   return (
-    <div>Todo</div>
+    <div className="flex flex-row bg-purple-600 w-full py-4 justify-between px-4 mb-4 text-white">
+      <p>{task}</p>
+      <div className='flex'>
+        <FontAwesomeIcon className='pr-4 cursor-pointer' icon={faPenToSquare} />
+        <FontAwesomeIcon className='cursor-pointer' icon={faTrash} />
+      </div>
+    </div>
   )
 }
 
